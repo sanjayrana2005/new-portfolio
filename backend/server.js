26d46c594c37ca180ef20f3749430a6defe964e2
Sanjyrana <Sanjayrana5113@gmail.com>
2025-11-24 23:02:48 +0530

    DB connected

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,28 @@
 const express = require("express");
+const connectDB = require("./Config/ConnectDB");
 const app = express();
 require("dotenv").config();
 
 
-app.use(
-    fileUpload({
-        useTempFiles:true,
-        tempFileDir:"/tmp/",
-    })
-);
+// app.use(
+//     fileUpload({
+//         useTempFiles: true,
+//         tempFileDir: "/tmp/",
+//     })
+// );
 
 app.use("/", (req, res) => {
     res.send("hi from server hh");
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running at port ${process.env.PORT}`);
-});
+
+connectDB()
+    .then(() => {
+        console.log("Database connected!!");
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running at port ${process.env.PORT}!!`);
+        });
+    })
+    .catch((error)=>{
+        console.log("Database can not connected!!")
+    })
