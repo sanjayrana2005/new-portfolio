d3a05e8d9f905894763e24cbcaaa87b48138c393
Sanjyrana <Sanjayrana5113@gmail.com>
2025-12-06 15:38:01 +0530

    about me

--- Frontend/src/pages/Sub components/About.jsx
+++ Frontend/src/pages/Sub components/About.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import axios from "axios";
 
 const About = () => {
-  return (
-    <div>
-      about
-    </div>
-  )
+    const [user, setUser] = useState({})
+
+    useEffect(() => {
+        const getMyProfile = async () => {
+            const { data } = await axios.get(`${import.meta.env.VITE_BACKEND_BASE_URL}/me`, {
+                withCredentials: true
+            });
+            setUser(data.user);
+        }
+        getMyProfile()
+    }, [])
+    return (
+        <div className='w-full flex flex-col overflow-x-hidden'>
+            <div className='relative'>
+                <h1 className='flex gap-4 items-center text-[2rem] sm:text-[2.75rem] md:text-[3rem] lg:text-[3.8rem] leading-14 md:leading-[67px] lg:leading-[90px] tracking-[15px] mx-auto w-fit font-extrabold ' style={{ background: "hsl(222.2 84% 4.9%" }}> ABOUT
+                    <span className='text-tubeLight-effect font-extrabold'>ME</span>
+                </h1>
+                <span className='absolute w-full h-1 top-7 sm:top-7 md:top-8 lg:top-11 z-[-1] bg-slate-200'></span>
+            </div>
+            <div>
+                <div className='grid md:grid-cols-2 my-8 sm:my-20 gap-20'>
+                    <div className='flex justify-center items-center'>
+                        <img src={user.avatar && user.avatar.url} alt={user.fullName} className='bg-white p-2 sm:p-4 rotate-25 h-60 sm:h[340px] md:h-[350px] lg:h-[450px]' />
+                    </div>
+                    <div className='flex justify-center
+                     flex-col tracking-[1px] text-xl gap-5'>
+                        <p>Hi, I’m Sanjy Rana, a passionate Full-Stack Developer with experience in React, Node.js, and MongoDB. I love building interactive, responsive, and user-friendly web applications.These days, I’m learning React Native to create mobile apps and improving my DSA skills to solve coding problems more efficiently.</p>
+                        <p>Take a look at my projects below to see what I’ve been working on — I’m always excited to connect, collaborate, and create meaningful digital experiences.</p>
+
+                    </div>
+                </div>
+                <p className='tracking-[1px] text-xl'>I’m eager to take up internships or job opportunities where I can apply my skills, gain industry experience, and contribute to impactful projects.</p>
+            </div>
+        </div>
+    )
 }
 
 export default About
--- Frontend/src/pages/Sub components/Timeline.jsx
+++ Frontend/src/pages/Sub components/Timeline.jsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react'
 
@@ -17,17 +16,17 @@ const Timeline = () => {
         <div>
             <h1 className='overflow-x-hidden text-[1.75rem] md:text-[2.2rem] lg:text-[2.8rem] mb-4 font-extrabold'>Timeline</h1>
 
-            <ol class="relative border-s border-default">
+            <ol className="relative border-s border-default">
                 {
                     timeline && timeline.map((time) => {
                         return (
-                            <li key={timeline._id} class="mb-10 ms-6">
-                                <span class="absolute flex items-center justify-center w-6 h-6 bg-brand-softer rounded-full -start-4 ring-4 ring-buffer">
-                                    <svg class="w-3 h-3 text-fg-brand-strong" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z" /></svg>
+                            <li key={time._id} className="mb-10 ms-6">
+                                <span className="absolute flex items-center justify-center w-6 h-6 bg-brand-softer rounded-full -start-4 ring-4 ring-buffer">
+                                    <svg className="w-3 h-3 text-fg-brand-strong" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 10h16m-8-3V4M7 7V4m10 3V4M5 20h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Zm3-7h.01v.01H8V13Zm4 0h.01v.01H12V13Zm4 0h.01v.01H16V13Zm-8 4h.01v.01H8V17Zm4 0h.01v.01H12V17Zm4 0h.01v.01H16V17Z" /></svg>
                                 </span>
-                                <h3 class="my-2 text-lg font-bold text-heading">{time.title}</h3>
-                                <p class="text-body text-gray-400">{time.description}</p>
-                                <time class="bg-neutral-secondary-medium border border-default-medium text-heading text-xs font-medium px-1.5 py-0.5 rounded text-gray-400">{time.timeline.from} - {time.timeline.to ? time.timeline.to : "Present"}</time>
+                                <h3 className="my-2 text-lg font-bold text-heading">{time.title}</h3>
+                                <p className="text-body text-gray-400">{time.description}</p>
+                                <time className="bg-neutral-secondary-medium border border-default-medium text-heading text-xs font-medium px-1.5 py-0.5 rounded text-gray-400">{time.timeline.from} - {time.timeline.to ? time.timeline.to : "Present"}</time>
                             </li>
                         )
                     })
