73e4336774f291e10c9bc9c68833cbfee4cf659d
Sanjyrana <Sanjayrana5113@gmail.com>
2025-12-06 11:33:15 +0530

    shadcn and route setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import { ThemeProvider } from './components/ui/theme-provider'
+import { Children } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className=''>
-      hi
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Routes>
+            <Route  path='/' element={<Home/>} />
+            <Route  path='/project/:id' element={<ProjectView/>} />
+        </Routes>
+        <Footer/>
+      </Router>
+    </ThemeProvider>
   )
 }
 
